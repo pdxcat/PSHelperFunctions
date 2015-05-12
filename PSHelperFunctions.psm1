210c23de8b7d6954372f7dd6d9bc0ee66a1dca36
Craig Meinschein <cmeinsch@gmail.com>
2015-05-12 16:38:17 -0700

    Added first commit of functions.

---
+++ PSHelperFunctions.psm1
@@ -0,0 +1,20 @@
+﻿# An assortment of helper functions that are useful in the CAT/MCECS environment.
+Function Test-32Bit {
+    return [IntPtr]::size -eq 4
+}
+
+Function Assert-32Bit {
+    $Is32Bit = Test-32Bit
+    if (-not $is32Bit) { throw "Shell is not 32-bit.`n" }
+    return $Is32Bit
+}
+
+Function Test-64Bit {
+    return [IntPtr]::size -eq 8
+}
+
+Function Assert-64Bit {
+    $Is64Bit = Test-64Bit
+    if (-not $is64Bit) { throw "Shell is not 64-bit.`n" }
+    return $Is64Bit
+}
